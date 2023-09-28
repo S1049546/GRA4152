4202a354eec0a06260be14a86d4b435670e4bb51
S1049546 <s2312390@bi.no>
2023-09-28 14:08:51 +0200

    changed fit_intercept

--- argparsing_logreg.py
+++ argparsing_logreg.py
@@ -2,7 +2,7 @@
 import argparse
 parser = argparse.ArgumentParser(description="Program creates a regression classifier, 4 arguments, being: Penalty(int), fit_intercept(boolean), max_iter(int) and tol(float)", epilog="https://scikit-learn.org/stable/modules/generated/sklearn.linear_model.LogisticRegression.html")
 parser.add_argument('--penalty', type=str, default='l2',choices=["l1", "l2", "None, elasticnet"], help="Here you write the penalty")
-parser.add_argument('--fit_intercept', type=bool, action='store_true', help="Here you write the intercept of the model")
+parser.add_argument('--fit_intercept', action='store_true', help="Here you write the intercept of the model")
 parser.add_argument('--max_iter', type=int, default=100, help="Max number of iterations")
 parser.add_argument('--tol', type=float, default=1e-4, help="Tolerance for stopping")
 
